69ab811653dc36fb04c22c310312c76168b22711
“ewhite1999” <ewhite1999@gmail.com>
2022-01-31 11:48:42 +0000

    fix typo

--- client/app.js
+++ client/app.js
@@ -23,6 +23,30 @@ let fakeData = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac consectetur nunc. Maecenas vulputate enim non lorem consectetur, at vulputate diam mollis. Integer congue accumsan lectus vel dignis",
     tags: "happy",
   },
+  {
+    to: "to one",
+    message:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac consectetur nunc. Maecenas vulputate enim non lorem consectetur, at vulputate diam mollis. Integer congue accumsan lectus vel dignis",
+    tags: "sad",
+  },
+  {
+    to: "to two",
+    message:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac consectetur nunc. Maecenas vulputate enim non lorem consectetur, at vulputate diam mollis. Integer congue accumsan lectus vel dignis",
+    tags: "truth",
+  },
+  {
+    to: "to three",
+    message:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac consectetur nunc. Maecenas vulputate enim non lorem consectetur, at vulputate diam mollis. Integer congue accumsan lectus vel dignis",
+    tags: "sad",
+  },
+  {
+    to: "to four",
+    message:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac consectetur nunc. Maecenas vulputate enim non lorem consectetur, at vulputate diam mollis. Integer congue accumsan lectus vel dignis",
+    tags: "happy",
+  },
 ];
 
 const createTo = (to) => {
@@ -35,7 +59,6 @@ const createTo = (to) => {
 };
 
 const createMessage = (body) => {
-  // TODO: check that there is no other h2s, if is change this to a h3
   let message = document.createElement("p");
   message.classList.add("message_elem", "card_child");
   message.innerText = body;
@@ -44,7 +67,6 @@ const createMessage = (body) => {
 };
 
 const createTag = (tag) => {
-  // TODO: check that there is no other h2s, if is change this to a h3
   let tagElem = document.createElement("div");
   tagElem.classList.add("tagElem_elem", "card_child");
   tagElem.innerText = tag;
