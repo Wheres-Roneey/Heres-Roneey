c4a82394700008f4ca07374137a287dd228035aa
r0neel <ronbhagwakar@gmail.com>
2022-01-31 12:34:28 +0000

    add post method

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const messages = require("./messages.json");
+const fs = require("fs");
+const fileName = "./messages.json";
+const messages = require(fileName);
+
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -16,6 +20,23 @@ app.get("/messages/:id", (req, res) => {
   let messageId = req.params.id;
   let index = parseInt(messageId);
   res.json(messages[index]);
+});
+
+app.post("/messages", (req, res) => {
+  let newMessage = {
+    to: req.body.to,
+    body: req.body.body,
+    tags: [req.body.tags],
+  };
+  messages.push(newMessage);
+  fs.writeFile(fileName, JSON.stringify(messages), (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+
+  res.send("message added");
 });
 
 module.exports = app;
