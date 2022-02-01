b7822981a580fec71a58917736103f2de7315a0a
“ewhite1999” <ewhite1999@gmail.com>
2022-02-01 15:47:19 +0000

    filter by tag

--- client/bundle.js
+++ client/bundle.js
@@ -242,4 +242,23 @@ const loadPage = async () => {
 
 loadPage();
 
+const btns = document.querySelectorAll(".btns");
+btns.forEach((btn) => {
+  btn.addEventListener("click", async (e) => {
+    const tagTarget = e.target.innerText.slice(1);
+    const response = await fetch(
+      `http://localhost:3000/messages/tags/${tagTarget}`
+    );
+    const data = await response.json();
+    data.forEach((card, index) => {
+      let to = card["to"];
+      let message = card["body"];
+      let tags = card["tags"];
+
+      createCard(index, to, message, tags);
+    });
+    addCard();
+  });
+});
+
 },{"./cards":1,"./lightMode":4}]},{},[5]);
