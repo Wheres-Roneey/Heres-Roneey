0ad29721707b30a23a962cb490629bbe0a642951
Phil99fp <philipcotter42@gmail.com>
2022-02-02 12:10:41 +0000

    giphy element created

--- client/giphyapi.js
+++ client/giphyapi.js
@@ -1,21 +1,23 @@
-// const { response } = require("express");
-
-// function sendApiRequest(){
-//   let giphyInput = document.getElementById("input").value
-//   console.log(giphyInput)
-
 const form = document.querySelector("form");
-form.addEventListener("click", (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  giphySearch(e.target.value);
+  giphySearch(e.target.querySelector("#search").value);
 });
 
 const giphyKey = "UTn30CTrQ5AweWYK7c50BaP6Fd28hUr3";
 
-function giphySearch(keyword) {
-  fetch(`http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${giphyKey}`)
-    .then((resp) => resp.json())
-    .then((data) => console.log(data));
+async function giphySearch(keyword) {
+  const resp = await fetch(
+    `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${giphyKey}`
+  );
+  const jsonData = await resp.json();
+  console.log(jsonData);
+  console.log("META", jsonData.meta);
+  let img = document.createElement("img");
+  img.src = jsonData.data[0].images.downsized.url;
+  let out = document.querySelector(".out");
+  out.insertAdjacentElement("afterbegin", img);
+  document.querySelector("#search").value = " ";
 }
 
 module.exports = { giphySearch };
