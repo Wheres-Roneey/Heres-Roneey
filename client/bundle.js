4688f03c17bb99a1fa17bd03cea9d6b415ad25c8
Phil99fp <philipcotter42@gmail.com>
2022-02-02 15:08:52 +0000

    giphy error handling

--- client/bundle.js
+++ client/bundle.js
@@ -289,17 +289,36 @@ form.addEventListener("submit", (e) => {
 const giphyKey = "UTn30CTrQ5AweWYK7c50BaP6Fd28hUr3";
 
 async function giphySearch(keyword) {
-  const resp = await fetch(
-    `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${giphyKey}`
-  );
-  const jsonData = await resp.json();
-  console.log(jsonData);
-  console.log("META", jsonData.meta);
-  let img = document.createElement("img");
-  img.src = jsonData.data[0].images.downsized.url;
-  let out = document.querySelector(".out");
-  out.insertAdjacentElement("afterbegin", img);
-  document.querySelector("#search").value = " ";
+  try {
+    const resp = await fetch(
+      `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${giphyKey}`
+    );
+    const jsonData = await resp.json();
+    // console.log(jsonData);
+    // console.log("META", jsonData.meta);
+    if (jsonData.data.length == 0) {
+      throw new Error("not found");
+    }
+    let img = document.createElement("img");
+    img.src = jsonData.data[0].images.downsized.url;
+    let out = document.querySelector(".out");
+    out.insertAdjacentElement("afterbegin", img);
+    document.querySelector("#search").value = " ";
+  } catch (err) {
+    nf = "not found";
+    console.log(err.message);
+    const resp1 = await fetch(
+      `http://api.giphy.com/v1/gifs/search?q=${nf}&api_key=${giphyKey}`
+    );
+    const jsonData1 = await resp1.json();
+    // console.log(jsonData1);
+    // console.log("META", jsonData1.meta);
+    let img = document.createElement("img");
+    img.src = jsonData1.data[0].images.downsized.url;
+    let out = document.querySelector(".out");
+    out.insertAdjacentElement("afterbegin", img);
+    document.querySelector("#search").value = " ";
+  }
 }
 
 module.exports = { giphySearch };
