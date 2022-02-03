8e6d2120af09d1e4d45b7643e94c554c8c1d4714
“ewhite1999” <ewhite1999@gmail.com>
2022-02-03 09:37:13 +0000

    server handling

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require("./app.js");
 const port = 3000;
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Express now departing from port ${port}`);
 });
