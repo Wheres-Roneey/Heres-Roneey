df780b1a8bcd554112910986da292dd0dcbf94de
“ewhite1999” <ewhite1999@gmail.com>
2022-02-03 10:34:49 +0000

    broken server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,13 @@ const app = require("./app.js");
 
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
+
 app.listen(port, () => {
   console.log("App is running on port " + port);
 });
